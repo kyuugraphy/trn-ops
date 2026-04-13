bf39619c86057d97a5ec8c75e0daf5d1df330bfe
kyuugraphy <echo.of.psycho@gmail.com>
2026-04-13 13:20:30 +0200

    del icons from nav bar

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ st.set_page_config(
 pages = {
     "Navigation": [
         st.Page("home.py", title="Home", icon="🏠"),
-        st.Page("pages/1_Manual_Accounts.py", title="Manual Accounts", icon="📝"),
-        st.Page("pages/2_Transaction_Labeling.py", title="Transaction Labeling", icon="🏷️"),
+        st.Page("pages/1_Manual_Accounts.py", title="Manual Accounts"),
+        st.Page("pages/2_Transaction_Labeling.py", title="Transaction Labeling"),
     ]
 }
 
